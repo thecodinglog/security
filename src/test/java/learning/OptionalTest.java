7fb52b1c11a9b92fd3d680dc1f12d4a27ae149d9
Jeongjin Kim <thefuturecreator@gmail.com>
2018-08-01 19:08:40 +0900

    Learning Test 정리

--- src/test/java/learning/OptionalTest.java
+++ src/test/java/learning/OptionalTest.java
@@ -1,3 +1,6 @@
+package learning;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Optional;
@@ -7,7 +10,7 @@ import java.util.Optional;
  * @since 2018. 7. 31.
  */
 
-public class learningTests {
+public class OptionalTest {
 
     class House{
         private String name;
@@ -33,13 +36,12 @@ public class learningTests {
     }
 
     @Test
+    @Ignore("Learning test")
     public void optional(){
         House house = null;
         Optional<House> houseOpt = Optional.ofNullable(house);
         Optional<Building> buildingOpt = houseOpt.map(house1 -> new Building());
 
         Building b1 = buildingOpt.orElse(null);
-
-
     }
 }
