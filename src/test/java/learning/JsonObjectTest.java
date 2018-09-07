35b2febce6c974b59f23ef89a0d95445c528a400
Jeongjin Kim <thefuturecreator@gmail.com>
2018-09-07 10:55:19 +0900

    파일 인코딩 추가

--- src/test/java/learning/JsonObjectTest.java
+++ src/test/java/learning/JsonObjectTest.java
@@ -2,6 +2,7 @@ package learning;
 
 import com.google.gson.Gson;
 import cothe.security.access.PermissionDescription;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.*;
@@ -11,13 +12,15 @@ import java.util.Map;
  * @author Jeongjin Kim
  * @since 2018. 8. 17.
  */
+@Ignore
 public class JsonObjectTest {
 
 
     @Test
     public void jsonToMap(){
         Gson gson = new Gson();
-        try(Reader fr = new InputStreamReader(getClass().getResourceAsStream("/permissionsJson/allowNormal.json"))){
+        try(Reader fr = new InputStreamReader(getClass().getResourceAsStream("/permissionsJson/allowNormal.json"),
+                "utf-8")){
             Map data = gson.fromJson(fr, Map.class);
             System.out.println(data);
         } catch (IOException e) {
@@ -29,7 +32,8 @@ public class JsonObjectTest {
     @Test
     public void jsonToService(){
         Gson gson = new Gson();
-        try(Reader fr = new InputStreamReader(getClass().getResourceAsStream("/permissionsJson/allowNormal.json"))){
+        try(Reader fr = new InputStreamReader(getClass().getResourceAsStream("/permissionsJson/allowNormal.json"),
+                "utf-8")){
             PermissionDescription data = gson.fromJson(fr, PermissionDescription.class);
             System.out.println(data);
         } catch (IOException e) {
